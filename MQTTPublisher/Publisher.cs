1cfd1e6486172de8aa3555dad6908d7f538bf023
raghavrawat98 <49871652+raghavrawat98@users.noreply.github.com>
2022-12-09 04:47:40 +0530

    Creating Publisher

---
+++ MQTTPublisher/Publisher.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet;
+using MQTTnet.Client;
+using System;
+
+namespace MQTTPublisher {
+    class Publisher{
+        public static async Task Main(String[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer("test.mosquitto.org",1883)
+                .WithCleanSession()
+                .Build();
+
+            
+
+           await client.ConnectAsync(options);
+
+            Console.WriteLine("Pree any key to oublish a message...");
+            Console.ReadLine();
+
+            await publishMessageAsyncClient(client);
+
+            // as soon as message is published, disconnect the client
+
+            await client.DisconnectAsync();
+           
+        }
+
+        private static async Task publishMessageAsyncClient(IMqttClient client)
+        {
+            string messagePayload = "I said Helllo";
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("MyTopic")           //Name of topic where message is published
+                .WithPayload(messagePayload)
+                .WithQualityOfServiceLevel(0)   //Qos 0 1 2 3
+                .Build();
+
+            if (client.IsConnected) {
+                await client.PublishAsync(message);
+            }
+
+        }
+    }
+}
+
+
